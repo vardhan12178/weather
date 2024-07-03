2d71afc2d61c494c45ded1edb206c77d86f03ede
vardhan12178 <balavardhan12178@gmail.com>
2024-07-03 12:43:44 +0530

    Modified

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => {
-  const text = "Developed by Bala Vardhan | Powered by React © 2024";
+  const text = "© Bala Vardhan @2024";
   
   return (
     <footer className="w-full py-2 mt-5 text-sm bg-gradient-to-r from-blue-800 to-purple-800 dark:from-gray-700 dark:to-gray-600 text-white dark:text-gray-200 text-center shadow-lg">
--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -5,7 +5,7 @@ const NotFound = () => {
   return (
     <div className="flex justify-center items-center  dark:from-black dark:to-black h-90">
       <div className="bg-gradient-to-r from-blue-500 to-purple-600 dark:from-gray-800 dark:to-gray-700 p-6 rounded-xl shadow-lg text-center text-white max-w-md w-full rounded-xl"> {/* Added rounded-xl here */}
-        <h2 className="text-2xl sm:text-3xl font-semibold mb-4">Location Not Found</h2>
+        <h2 className="text-2xl sm:text-2xl font-semibold mb-4">Location Not Found</h2>
         <p className="text-lg mb-4">Please enter a valid city name.</p>
         <ExclamationCircleIcon className="w-12 h-12 sm:w-16 sm:h-16 text-white mb-6 mx-auto" />
       </div>
